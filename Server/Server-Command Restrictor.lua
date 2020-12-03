b639bcb320702ce42c0a60ba2abf5cef12afb495
Sceleratis <sceleratis@gmail.com>
2020-12-03 13:31:59 -0500

    Update Server-Command Restrictor.lua

--- Server/Server-Command Restrictor.lua
+++ Server/Server-Command Restrictor.lua
@@ -5,35 +5,105 @@ return function()
 	--// If the value is false, it will use the command's original AdminLevel
 	--// If the value is set to a string it will use that as the new AdminLevel
 	--// Any commands not in the below table will be set as Creators only
+	--// You should be able to use either the command index or one of the command's "Commands" strings
 	
-	local whitelist = {
-		To = false;
-		Bring = false;
-		Teleport = false;
-		ResetView = false;
-		Message = false;
-		Notif = false;
-		Hint = false;
-		Notify = false;
-		Notification = false;
-		HintCountdown = false;
-		PrivateMessage = false;
+	local commands = { --// Command string or index
 		Shutdown = "Owners";
-		Invisible = "Admins";
+		Invisible = "Owners";
 		Respawn = "Admins";
-		Visible = "Admins";
-		Shirt = "Admins";
-		Pants = "Admins";
-		Kick = "Admins";
-		Ban = "Owners";
-		View = "Owners";
+		Visible = "Owners";
+		Shirt = "Owners";
+		Pants = "Owners";
+		Kick = "Moderators";
+		Ban = "Admins";
+		View = "Moderators";
+		chat = "Admins";
+		unchat = "Admins";
+		N = "Admins";
+		Clean = "Moderators";
+		Jump = "Moderators";
+		Tban = "Admins";
+		Mpm = "Moderators";
+		Admins = "Moderators"; 
+		Untimeban = "Admins";
+		Team = "Moderators";
+		Joinlogs = "Moderators";
+		Pnum = "Moderators";
+		notepad = "Moderators";
+		Note = "Moderators";
+		Hcountdown = "Admins";
+		Stopcountdown = "Admins";
+		Bring = "Moderators";
+		mutelist = "Moderators";
+		Refresh = "Moderators";
+		m = "Admins";
+		Setmessage = "Admins";
+		To = "Moderators";
+		gear = "Admins";
+		notes = "Moderators";
+		removenotes = "Moderators";
+		countdown = "Admins";
+		view = "Moderators";
+		players = "Moderators";
+		flynoclip = "Admins";
+		notify = "Admins";
+		details = "Moderators";
+		gameban = "Admins";
+		clip = "Admins";
+		bchat = "Admins";
+		unname = "Admins";
+		kill = "Admins";
+		track = "Moderators";
+		logs = "Moderators";
+		mute = "Moderators";
+		ungameban = "Admins";
+		respawn = "Moderators";
+		chatlogs = "Moderators";
+		info = "Moderators";
+		showlogs = "Moderators";
+		clear = "Moderators";
+		tp = "Moderators";
+		quote = "Moderators";
+		to = "Moderators";
+		noclip = "Admins";
+		fly = "Admins"; 
+		unfly = "Admins";
+		cmds = "Moderators";
+		exploitlogs = "Moderators";
+		cmdbox = "Moderators";
+		kick = "Moderators";
+		ban = "Admins";
+		name = "Admins";
+		Chatnotify = "Moderators";
+		warnings = "Moderators";
+		timebanlist = "Moderators";
+		banlist = "Moderators";
 	}
 	
-	for index,com in next,server.Commands do
-		if whitelist[index] ~= nil then
-			if whitelist[index] then
-				com.AdminLevel = whitelist[index]
+	
+	local whitelist = {} --// Add ONLY the command indecies to this (if you need to target a specific index)
+	
+	local function getIndex(str)
+		for index,cmd in next,server.Commands do
+			for i,c in next,cmd.Commands do
+				if c:lower() == str:lower() then
+					return index
+				end
 			end
+		end
+	end
+	
+	for i,v in next,commands do
+		local found = server.Commands[i] or getIndex(i)
+		if found then
+			whitelist[i] = v
+		end
+	end
+
+	for index,com in next,server.Commands do
+		local newLevel = whitelist[index];
+		if whitelist[index] ~= nil then
+			com.AdminLevel = ((type(newLevel) == "string" or type(newLevel) == "table") and newLevel) or com.AdminLevel
 		else
 			com.AdminLevel = "Creators"
 		end

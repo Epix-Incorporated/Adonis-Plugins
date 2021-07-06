98b8bb2225c57c4cb6880aae43a7fe2e929fe222
Sky <sceleratis@gmail.com>
2021-07-06 15:19:34 -0400

    Update Server-GivePrivateServerOwnerAdmin.lua

--- Server/Server-GivePrivateServerOwnerAdmin.lua
+++ Server/Server-GivePrivateServerOwnerAdmin.lua
@@ -3,7 +3,7 @@
 --// Source: https://devforum.roblox.com/t/adonis-g-api-usage/477194/4
 
 --// Owners can do :permadmin, so it's unsafe to to make them owners unless you change the perm levels for those commands or disable SaveAdmins in the settings
-local AdminLevel = 2; --// 1 - Mod, 2 - Admin, 3 - Owner
+local AdminLevel = 200; --// 100 - Mod, 200 - Admin, 300 - HeadAdmin
 
 return function()
   service.Players.PlayerAdded:Connect(function(p)

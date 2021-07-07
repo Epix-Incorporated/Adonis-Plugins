2937e8d57889e86214777def18b370daf2f0e26e
Sceleratis <sceleratis@gmail.com>
2021-07-07 15:05:40 -0400

    Update Server-PlayerFinder_Restrictor.lua

--- Server/Server-PlayerFinder_Restrictor.lua
+++ Server/Server-PlayerFinder_Restrictor.lua
@@ -1,12 +1,13 @@
 --[[
 	Author: Sceleratis/Davey_Bones
 	Description: This plugin will restrict any PlayerFinders (all, others, me, etc) to a specific admin level only
-	
+
 	Place ModuleScript in Adonis_Loader > Config > Plugins and name it "Server-PlayerFinder Restrictor"
 --]]
 
 
 local RestrictToLevel = 100; --// Players must be level X or higher in the admin ranks
+local ProtectedLevels = {900, 1000}; --// Levels that are always excluded from playerfinders listed below
 local PlayerFinders = {  --// Player finders to restrict
   "all",
   "others",
@@ -31,9 +32,17 @@ return function()
     local finder = Finders[ind];
     if finder then
       local oldFunc = finder.Function;
-      finder.Function = function(msg, plr, ...)
+      finder.Function = function(msg, plr, par, players, ...)
         if Admin.GetLevel(plr) >= RestrictToLevel then
-          return oldFunc(msg, plr, ...);
+          oldFunc(msg, plr, par, players, ...);
+
+          for i,p in ipairs(players) do
+            for k,rank in ipairs(ProtectedLevels) do
+              if Admin.GetLevel(p) == rank then
+                table.remove(players, i)
+              end
+            end
+          end
         end
       end
     end

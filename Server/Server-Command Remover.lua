823544068d4befa75f72a8c273549de6959ce323
ALE111_boiPNG <68124053+ccuser44@users.noreply.github.com>
2021-09-30 17:13:02 +0300

    Update Server-Command Remover.lua

--- Server/Server-Command Remover.lua
+++ Server/Server-Command Remover.lua
@@ -10,61 +10,61 @@ server = nil
 return function()
 	local prefix = server.Settings.Prefix; 			--// By default this is ":" and is used for most commands
 	local sPrefix = server.Settings.PlayerPrefix; 	--// By default this is "!" and is used for Player commands
-	
+
 	--// Finds commands that match below (as they would be chatted in-game)
 	local RemoveList_WithPrefix = {
 		prefix .. "kill"; --// :kill
 		sPrefix .. "hat"; --// !hat
 	}
-	
+
 	--// Finds commands listed below, regardless of their prefix. Adding "hat" to this table would remove both !hat and :hat (even tho they aren't the same command)
 	--// You can use this one if you don't know which prefix is used by the target command.
 	local RemoveList_NoPrefix = {
 		"fire"; --// !fire (Donors) and :fire <player> (Admins)
 	}
-	
+
 	local function checkAlias_Prefix(cmd, match)
-		for i,v in next,cmd.Commands do
-			if (cmd.Prefix .. v):lower() == match:lower() then
-				return true;
-			end
-		end
-		
-		return false;
-	end
-	
-	local function checkAlias_NoPrefix(cmd, match)
-		for i,v in next,cmd.Commands do
-			if v:lower() == match:lower() then
+		for _, v in pairs(cmd.Commands) do
+			if string.lower(cmd.Prefix .. v) == string.lower(match) then
 				return true;
 			end
 		end
 
 		return false;
 	end
-	
+
+	local function checkAlias_NoPrefix(cmd, match)
+		for _, v in pairs(cmd.Commands) do
+			if string.lower(v) == string.lower(match) then
+				return true;
+			end
+		end
+
+		return false;
+	end
+
 	local function checkList(cmd)
-		for i,v in next, RemoveList_WithPrefix do 
+		for _, v in ipairs(RemoveList_WithPrefix) do 
 			if checkAlias_Prefix(cmd, v) then
 				return true;
 			end
 		end
-		
-		for i,v in next, RemoveList_NoPrefix do 
+
+		for _, v in ipairs(RemoveList_NoPrefix) do 
 			if checkAlias_NoPrefix(cmd, v) then
 				return true;
 			end
 		end
-		
+
 		return false;
 	end
-	
-	for index,com in next,server.Commands do
+
+	for key, com in pairs(server.Commands) do
 		if checkList(com) then
-			server.Commands[index] = nil;
-			warn("Removed ".. tostring(index) .." from commands table");
+			server.Commands[key] = nil;
+			warn("Removed ".. tostring(key) .." from commands table");
 		end
 	end
-	
+
 	server.Admin.CacheCommands();
 end

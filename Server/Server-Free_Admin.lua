54186099c5655ca036bd2f22f354a067ed61d61d
Expertcoderz <81153405+Expertcoderz@users.noreply.github.com>
2022-07-03 00:10:11 +0800

    Update Server-Free_Admin.lua

--- Server/Server-Free_Admin.lua
+++ Server/Server-Free_Admin.lua
@@ -1,27 +1,13 @@
 --!nonstrict
 --[[
-  Place in a ModuleScript under Adonis_Loader > Config > Plugins
-  ModuleScript must be named: "Server: Free Admin"    (without the quotes)
+	Author: Sceleratis (Davey_Bones)
+	Description: This plugin gives admin to every non-admin player who joins
 
-  Name: Free Admin
-  Author: Sceleratis (Davey_Bones)
-  Description: Gives admin to every non-admin player that joins
+	Place in a ModuleScript under Adonis_Loader > Config > Plugins, named "Server-Free_Admin"
 --]]
 
-local FREE_ADMIN_LEVEL: number = 100 -- level of admin you want to give everyone; NOTE: Creator (level 900+) is EXTREMELY risky and not advised as they can do everything including change settings
-
---[[
-  Default Admin Levels:
-
-  Banned   -1
-  Player    0     (Non-admins - These players are not considered admins)
-  Moderator 100
-  Admin     200
-  HeadAdmin 300   (HeadAdmins are basically SuperAdmins)
-  Creator   900+  (Anything 900 or higher is considering game creator level and can do absolutely anything including edit settings in-game)
-
-  Note: This will also work for custom ranks if you specify their level.
---]]
+local FREE_ADMIN_LEVEL: number = 100 --// Level of admin to give everyone; rank levels are defined in loader Settings
+--// NOTE: Creator (level 900+) is EXTREMELY risky and not advised as they can do everything including change settings.
 
 return function(Vargs)
 	local server = Vargs.Server
@@ -32,13 +18,13 @@ return function(Vargs)
 	local Admin = server.Admin
 	local Core = server.Core
 
-	service.Players.PlayerAdded:Connect(function(p)
+	service.Events.PlayerAdded:Connect(function(p)
 		task.wait(1)
 		if not Admin.CheckAdmin(p) then
 			Admin.SetLevel(p, FREE_ADMIN_LEVEL)
 			Remote.MakeGui(p, "Notification", {
 				Title = "Notification";
-				Message = "You are a(n)"..Admin.LevelToListName(FREE_ADMIN_LEVEL)..". Click to view commands.";
+				Message = string.format("You are a(n) %s. Click to view commands.", Admin.LevelToListName(FREE_ADMIN_LEVEL));
 				Time = 10;
 				Icon = server.MatIcons.Verified;
 				OnClick = Core.Bytecode("client.Remote.Send('ProcessCommand','"..Settings.Prefix.."cmds')");

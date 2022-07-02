9973fde417e30eca367c24f707d9e97b243c3899
Expertcoderz <81153405+Expertcoderz@users.noreply.github.com>
2022-07-02 23:22:17 +0800

    Update and rename Server-GivePrivateServerOwnerAdmin.lua to Server-PrivateServerOwnerAdmin.lua

---
+++ Server/Server-PrivateServerOwnerAdmin.lua
@@ -0,0 +1,22 @@
+--!strict
+--[[
+	Original Author: TacoBellSaucePackets
+	Original Source: https://devforum.roblox.com/t/adonis-g-api-usage/477194/4
+	Description: This plugin will give a certain temporary permission level to private server owners
+
+	Place this ModuleScript in Adonis_Loader > Config > Plugins and name it "Server-PrivateServerOwnerAdmin"
+--]]
+
+--// HeadAdmins can do :permadmin, so it's unsafe to to make them head admins unless you change the perm levels for those commands or disable SaveAdmins in the settings
+local PRIVATE_SERVER_OWNER_ADMIN_LEVEL: number = 200 --// (admin levels are defined in loader Settings)
+
+return function(Vargs)
+	local server = Vargs.Server
+	local service = Vargs.Service
+
+	service.Events.PlayerAdded:Connect(function(plr: Player)
+		if game.PrivateServerOwnerId == plr.UserId then
+			server.Admin.SetLevel(plr, PRIVATE_SERVER_OWNER_ADMIN_LEVEL)
+		end
+	end)
+end

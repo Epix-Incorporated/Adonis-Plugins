a4273c0763056ba5e560b89051b95632e0a2cae1
Expertcoderz <81153405+Expertcoderz@users.noreply.github.com>
2022-07-03 09:09:48 +0800

    Update and rename Server-Trello Command Logs.lua to Server-Trello_CommandLogs.lua

---
+++ Server/Server-Trello_CommandLogs.lua
@@ -0,0 +1,51 @@
+--!nonstrict
+--[[
+	Author: Sceleratis/Davey_Bones
+	Description: This plugin will log command executions to a list in the primary Trello board configured in Loader settings
+
+	Place this ModuleScript in Adonis_Loader > Config > Plugins and name it "Server-Trello_CommandLogs"
+--]]
+
+local LOG_LIST_NAMES: {string} = {"Logs", "Admin Logs", "Command Logs"}
+
+return function(Vargs)
+	local server = Vargs.Server
+	local service = Vargs.Service
+
+	local Settings = server.Settings
+	local HTTP = server.HTTP
+
+	local trello = HTTP.Trello.API(Settings.Trello_AppKey, Settings.Trello_Token)
+	local logList = trello.getListObj(trello.getLists(Settings.Trello_Primary), LOG_LIST_NAMES)
+
+	if not logList then
+		warn("TRELLO COMMAND LOGS PLUGIN ABORTED; LOG LIST NOT FOUND ON TRELLO BOARD")
+		return
+	end
+
+	service.Events.CommandRan:Connect(function(plr: Player, data)
+		trello.makeCard(
+			logList.id,
+			tostring(plr)..": "..data.Message,
+			string.format(
+			[[--------------------------------------------
+Player: %s
+Message: %s
+
+Command: %s
+Args: %s
+
+Time: %s
+Successful: %s
+Error: %s
+--------------------------------------------]],
+				service.FormatPlayer(plr),
+				tostring(data.Message),
+				tostring(data.Matched),
+				if data.Args and #data.Args > 0 then table.concat(data.Args, ", ") else "-",
+				tostring(service.GetTime()),
+				tostring(data.Success),
+				tostring(data.Error)
+			))
+	end)
+end

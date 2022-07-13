0bb7aabf4eb7f69339faa4a6ac14b60345523abf
Expertcoderz <81153405+Expertcoderz@users.noreply.github.com>
2022-07-13 20:35:23 +0800

    Update Server-Free_Admin.lua

--- Server/Server-Free_Admin.lua
+++ Server/Server-Free_Admin.lua
@@ -9,6 +9,8 @@
 local FREE_ADMIN_LEVEL: number = 100 --// Level of admin to give everyone; rank levels are defined in loader Settings
 --// NOTE: Creator (level 900+) is EXTREMELY risky and not advised as they can do everything including change settings.
 
+local ADMIN_NOTIFICATIONS: boolean = true --// Whether players are notified upon being ranked
+
 return function(Vargs)
 	local server = Vargs.Server
 	local service = Vargs.Service
@@ -22,13 +24,15 @@ return function(Vargs)
 		task.wait(1)
 		if not Admin.CheckAdmin(p) then
 			Admin.SetLevel(p, FREE_ADMIN_LEVEL)
-			Remote.MakeGui(p, "Notification", {
-				Title = "Notification";
-				Message = string.format("You are a(n) %s. Click to view commands.", Admin.LevelToListName(FREE_ADMIN_LEVEL));
-				Time = 10;
-				Icon = server.MatIcons.Verified;
-				OnClick = Core.Bytecode("client.Remote.Send('ProcessCommand','"..Settings.Prefix.."cmds')");
-			})
+			if ADMIN_NOTIFICATIONS then
+				Remote.MakeGui(p, "Notification", {
+					Title = "Notification";
+					Icon = server.MatIcons.Verified;
+					Message = string.format("You are a(n) %s. Click to view commands.", Admin.LevelToListName(FREE_ADMIN_LEVEL));
+					Time = 10;
+					OnClick = Core.Bytecode("client.Remote.Send('ProcessCommand','"..Settings.Prefix.."cmds')");
+				})
+			end
 		end
 	end)
 end

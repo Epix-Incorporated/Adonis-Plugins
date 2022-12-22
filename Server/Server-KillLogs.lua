03f11edc117af5426fbc67d8ebc825e2f930b104
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2022-12-22 12:11:30 +0200

    Update Server-KillLogs.lua

--- Server/Server-KillLogs.lua
+++ Server/Server-KillLogs.lua
@@ -4,7 +4,31 @@
 	Name: KillLogs module
 	Description: This plugin will add a killlogs command which keeps track of people who have died and for which reasons
 	Place in a ModuleScript under Adonis_Loader > Config > Plugins and named "Server-KillLogs"
+	License: MIT
 --]]
+--[[
+	MIT License
+
+	Copyright (c) 2021 Github@ccuser44
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+]]
 
 return function(Vargs)
 	local server, service = Vargs.Server, Vargs.Service

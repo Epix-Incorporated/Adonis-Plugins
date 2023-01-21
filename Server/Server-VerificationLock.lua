7eb1562e2e25a571088a59a633b1995c45637084
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2023-01-21 18:12:43 +0200

    Update Server-VerificationLock.lua

--- Server/Server-VerificationLock.lua
+++ Server/Server-VerificationLock.lua
@@ -134,6 +134,7 @@ return function(Vargs)
 		end
 	end
 
+	Variables.AltVerificationLock = false
 	server.Commands.VerificationLock = {
 		Prefix = server.Settings.Prefix;
 		Commands = {"altlock", "verificationlock", "enableantialt"};
@@ -160,7 +161,6 @@ return function(Vargs)
 	};
 
 	server.Admin.CacheCommands()
-
 	service.HookEvent("PlayerAdded", function(player)
 		if Variables.AltVerificationLock == true then
 			if not isVerified(player) and not checkAdmin(player) then

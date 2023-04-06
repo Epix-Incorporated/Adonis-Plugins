33e2a30a4bda6b6e7f122612a72ad0d5bd6f4adc
WalkerOfBacon <50679909+WalkerOfBacon@users.noreply.github.com>
2023-04-06 15:44:09 -0400

    Update Server-Leaderstats.lua
    Added a disclaimer

--- Server/Server-Leaderstats.lua
+++ Server/Server-Leaderstats.lua
@@ -2,7 +2,8 @@
 --[[
 	Author: BaconOfWalker (Jumbo891011)
 	Name: Server-Leaderstats
-	Description: Adds :createstat and :removestat; Lets you create/remove leaderstats
+	Description: Adds :createstat and :removestat; Lets you create/remove leaderstats.
+	DISCLAIMER: This is still WIP because this will only work if there are no existing leaderstats.
 	Place in a ModuleScript under Adonis_Loader > Config > Plugins, named "Server-Leaderstats"
 --]]
 
@@ -14,7 +15,7 @@ return function(Vargs)
 	local Commands = server.Commands
 	local Functions = server.Functions
 	local Remote = server.Remote
-	
+
 	Commands.CreateLeaderstats = {
 		Prefix = Settings.Prefix;
 		Commands = {"createstat", "cstat", "cs"};
@@ -26,20 +27,27 @@ return function(Vargs)
 			local statName = assert(args[1], "Missing stat name")
 
 			for _, v in service.GetPlayers(plr, args[1]) do
-				local leaderstats = Instance.new("Folder")
-				leaderstats.Name = "leaderstats"
-				leaderstats.Parent = plr
+				local leaderstats = game.Players:FindFirstChild("leaderstats")
 
 				if leaderstats then
 					local statName = Instance.new("IntValue")
 					statName.Name = args[1]
 					statName.Value = 0
 					statName.Parent = leaderstats
+				else
+					local newStats = Instance.new("Folder")
+					newStats.Name = "leaderstats"
+					newStats.Parent = plr
+
+					local statName = Instance.new("IntValue")
+					statName.Name = args[1]
+					statName.Value = 0
+					statName.Parent = newStats
 				end
 			end
 		end
 	}
-	
+
 	Commands.RemoveLeaderstats = {
 		Prefix = Settings.Prefix;
 		Commands = {"removestat", "rstat", "rs"};
@@ -53,13 +61,13 @@ return function(Vargs)
 			for _, v in service.GetPlayers(plr, args[1]) do
 				local leaderstats = plr:FindFirstChild("leaderstats")
 
-				if leaderstats then
+				if leaderstats and (leaderstats:IsA("Folder") or leaderstats:IsA("IntValue")) then
 					local absoluteMatch = leaderstats:FindFirstChild(statName)
-					if absoluteMatch and (absoluteMatch:IsA("IntValue") or absoluteMatch:IsA("NumberValue")) then
+					if absoluteMatch and (absoluteMatch:IsA("IntValue") or absoluteMatch:IsA("NumberValue") or absoluteMatch:IsA("Folder")) then
 						leaderstats:Destroy()
 					else
 						for _, st in leaderstats:GetChildren() do
-							if (st:IsA("IntValue") or st:IsA("NumberValue")) and string.match(st.Name:lower(), "^"..statName:lower()) then
+							if (st:IsA("IntValue") or st:IsA("NumberValue") or st:IsA("Folder")) and string.match(st.Name:lower(), "^"..statName:lower()) then
 								leaderstats:Destroy()
 							end
 						end
